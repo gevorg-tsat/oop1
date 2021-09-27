c2b631dfc0c37a86c0ea41b9a03286ebbfb3349e
Геворг Цатурян <gstsaturyan@miem.hse.ru>
2021-09-28 00:25:01 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include <string>
 #include "Car.h"
+#include <cassert>
 int main() {
-    Car tachka = Car("BMW",10,10,10,0,0,10);
+    Car tachka = Car("BMW",10,10,10,0,0,45);
     Car tachkaDefault = Car();
+    Car tachkaCopy = Car(tachka);
+    assert(tachka.getName() == "BMW");
+    assert(tachka.getHeight() == 10);
+    assert(tachka.getWidth() == 10);
+    assert(tachka.getLength() == 10);
+    assert(tachka.getX() == 0);
+    assert(tachka.getY() == 0);
+    assert(tachka.getAngle() == 45);
+    assert(tachkaDefault.getName() == "");
+    assert(tachkaDefault.getHeight() == 0);
+    assert(tachkaDefault.getWidth() == 0);
+    assert(tachkaDefault.getLength() == 0);
+    assert(tachkaDefault.getX() == 0);
+    assert(tachkaDefault.getY() == 0);
+    assert(tachkaDefault.getAngle() == 0);
+    assert(tachkaCopy.getName() == tachka.getName());
+    assert(tachkaCopy.getHeight() == tachka.getHeight());
+    assert(tachkaCopy.getWidth() == tachka.getWidth());
+    assert(tachkaCopy.getLength() == tachka.getLength());
+    assert(tachkaCopy.getX() == tachka.getX());
+    assert(tachkaCopy.getY() == tachka.getY());
+    assert(tachkaCopy.getAngle() == tachka.getAngle());
+    tachka.setName("Mercedes");
+    assert(tachka.getName() == "Mercedes");
+
+    std::cout<<"Finished without errors"<<std::endl;
     return 0;
 }
